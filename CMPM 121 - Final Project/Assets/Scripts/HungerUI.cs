3dd3897f8b470fe72ddedc8bd9f6b5d471ad2ab4
jani9 <jacindani1@gmail.com>
2019-03-06 00:03:05 -0800

    Week 7 Update
    - Ability for player to feed fruitant
    - Basic UI for feeding (buttons)
    - Screen state management
    
    Still working on fixing the meter. For some reason the position keeps moving elsewhere.

---
+++ CMPM 121 - Final Project/Assets/Scripts/HungerUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungerUI : MonoBehaviour
+{
+    public GameObject pet;
+    public GameObject hungerBar;
+    private PetStats stats;
+
+    private int fullBar = -192;
+    private int empty = 0;
+    private float hungerPercent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hungerBar.transform.position = new Vector2(fullBar, hungerBar.transform.position.y);
+        stats = pet.GetComponent<PetStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeHungerBar();
+    }
+
+    void ChangeHungerBar()
+    {
+
+        hungerPercent = stats.currentHunger / 100f;
+        Debug.Log(hungerPercent);
+        hungerBar.GetComponent<RectTransform>().position = new Vector2(fullBar * hungerPercent, hungerBar.transform.position.y);
+        //hungerBar.transform.position = new Vector2 (fullBar * hungerPercent, hungerBar.transform.position.y);
+
+    }
+}
---
+++ CMPM 121 - Final Project/Assets/Scripts/PetStats.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PetStats : MonoBehaviour
+{
+    public Text hungerText;
+
+    public int currentHunger;
+    public int maxHunger = 100;
+    public int hungerRate = 5;
+    public int foodBuff = 10;
+
+    private float time = 0f;
+    private float rate = 2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHunger = maxHunger;
+        Debug.Log(currentHunger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > rate)
+        {
+            time = 0;
+            LowerHunger();
+            Debug.Log(currentHunger);
+        }
+        SetHungerText();
+
+    }
+
+    void SetHungerText()
+    {
+        hungerText.text = "Hunger: " + currentHunger.ToString();
+    }
+
+    void LowerHunger()
+    {
+        currentHunger = Mathf.Clamp(currentHunger - hungerRate, 0, maxHunger);
+
+    }
+
+    public void FeedPet()
+    {
+        currentHunger = Mathf.Clamp(currentHunger + foodBuff, 0, maxHunger);
+    }
+}
